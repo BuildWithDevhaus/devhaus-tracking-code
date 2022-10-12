9fe0bf53d2452f2297088cb95b4a68e3caa8ee08
Barjuan Davis <110009906+davisdevhaus@users.noreply.github.com>
2022-10-12 17:01:30 +0700

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,9 @@
  * Copyright(c) 2015 Douglas Christopher Wilson
  * MIT Licensed
  */
+
+/*
+    Devhaus Tracking Code v1.0.1
+    Copyright(c) 2022 by Davis @ Devhaus
+    Latest update: October 12th, 2022
+*/
