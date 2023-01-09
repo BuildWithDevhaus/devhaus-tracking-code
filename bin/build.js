649518c1b0e9866e4494d70f2d7a19daba5a5abd
Barjuan Davis <110009906+davisdevhaus@users.noreply.github.com>
2023-01-09 09:49:40 +0700

    Added geo tagging

--- bin/build.js
+++ bin/build.js
@@ -18,6 +18,7 @@ const defaultSettings = {
   sourcemap: !production,
   target: production ? 'es2017' : 'esnext',
   entryPoints,
+  footer: 'Copyright (c) 2023 Devhaus Pte Ltd',
 };
 
 // Files building
