7ce26aaa769b566d3058c1503ce2a99acfede127
Barjuan Davis <110009906+davisdevhaus@users.noreply.github.com>
2023-01-13 11:21:29 +0700

    removed interception for dev purposes

--- src/jotformTrackingCode.ts
+++ src/jotformTrackingCode.ts
@@ -3,62 +3,63 @@ import triggerSegmentEvent from 'utils/triggerSegmentEvent';
 import triggerSegmentIdentify from 'utils/triggerSegmentIdentify';
 
 export default function jotformTrackingCode() {
-  const subParent = document.querySelector('iframe');
+  const subParentArray = document.querySelectorAll('iframe');
   try {
-    const form = subParent?.contentWindow?.document?.querySelector?.('.jotform-form');
-    form?.addEventListener('submit', () => {
-      //e.preventDefault();
-      const inputs = [
-        ...form.querySelectorAll('input'),
-        ...form.querySelectorAll('select'),
-        ...form.querySelectorAll('textarea'),
-      ];
-      const data = {} as Record<string, string | string[]>;
-      inputs.forEach((input) => {
-        const name = input.getAttribute('name') ?? '';
-        const { value } = input;
+    subParentArray.forEach((subParent) => {
+      const form = subParent?.contentWindow?.document?.querySelector?.('.jotform-form');
+      //if form is a form, add event listener
+      if (form?.tagName !== 'FORM') return;
+      form?.addEventListener('submit', () => {
+        //e.preventDefault();
+        const inputs = [
+          ...form.querySelectorAll('input'),
+          ...form.querySelectorAll('select'),
+          ...form.querySelectorAll('textarea'),
+        ];
+        const data = {} as Record<string, string | string[]>;
+        inputs.forEach((input) => {
+          const name = input.getAttribute('name') ?? '';
+          const { value } = input;
 
-        const putData = () => {
-          const nameArray = name.split('_');
-          nameArray.shift();
-          const nameStringTemp = nameArray.join('_');
-          //remove the last [] from the name if it exists
-          const nameString = nameStringTemp.replace(/\[\]$/, '');
+          const putData = () => {
+            const nameArray = name.split('_');
+            nameArray.shift();
+            const nameStringTemp = nameArray.join('_');
+            const nameString = nameStringTemp.replace(/\[\]$/, '');
 
-          if (data[nameString]) {
-            //currently, name is still in q[number]_[name] format
-            //we need to convert it to [name] format
-
-            if (typeof data[nameString] === 'string') {
-              data[nameString] = [data[nameString], value] as string[];
+            if (data[nameString]) {
+              if (typeof data[nameString] === 'string') {
+                data[nameString] = [data[nameString], value] as string[];
+              } else {
+                (data[nameString] as string[]).push(value);
+              }
             } else {
-              (data[nameString] as string[]).push(value);
+              data[nameString] = value;
             }
-          } else {
-            data[nameString] = value;
-          }
-        };
+          };
 
-        if (name.search(/q\d+_[a-z\_]+\[?\]?/) !== -1) {
-          if (
-            (input.getAttribute('type') === 'checkbox' || input.getAttribute('type') === 'radio') &&
-            (input as HTMLInputElement).checked
-          ) {
-            putData();
-          } else if (
-            input.getAttribute('type') !== 'checkbox' &&
-            input.getAttribute('type') !== 'radio'
-          ) {
-            putData();
+          if (name.search(/q\d+_[a-z\_]+\[?\]?/) !== -1) {
+            if (
+              (input.getAttribute('type') === 'checkbox' ||
+                input.getAttribute('type') === 'radio') &&
+              (input as HTMLInputElement).checked
+            ) {
+              putData();
+            } else if (
+              input.getAttribute('type') !== 'checkbox' &&
+              input.getAttribute('type') !== 'radio'
+            ) {
+              putData();
+            }
           }
-        }
+        });
+        triggerSegmentIdentify(data);
+        triggerSegmentEvent(
+          subParent?.title ? `${subParent.title} Form Submitted` : 'Jotform Form Submitted',
+          removePIIsFromData(data)
+        );
+        //return false;
       });
-      triggerSegmentIdentify(data);
-      triggerSegmentEvent(
-        subParent?.title ? `${subParent.title} Form Submitted` : 'Jotform Form Submitted',
-        removePIIsFromData(data)
-      );
-      // return false;
     });
   } catch (e) {}
 }
--- src/utils/removePIIsFromData.ts
+++ src/utils/removePIIsFromData.ts
@@ -1,6 +1,6 @@
 export default function removePIIsFromData(data: Record<string, string | string[]>) {
   const dataCopy = { ...data };
-  const piiFields = ['email', 'name', 'first_name', 'last_name', 'phone'];
+  const piiFields = ['email', 'name', 'first_name', 'last_name', 'phone_number'];
   piiFields.forEach((piiField) => {
     if (dataCopy[piiField]) {
       delete dataCopy[piiField];
