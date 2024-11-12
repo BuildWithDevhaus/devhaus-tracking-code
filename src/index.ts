4b15fce9833e7e1b7fca3b1be2d46279cdd2893d
Barjuan Davis <davis@devhaus.com.sg>
2024-11-12 13:00:35 +0700

    fix consent manager integration to support is-dev and devwritekey

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,27 @@ import loadSegmentAnalytics from 'utils/loadSegmentAnalytics';
 
 const scriptTag = document.getElementById('devhaus-tracking-code');
 //check if script tag has segment-production-write-key,segment-dev-write-key, and enable-consent-manager attributes
-const segmentProductionWriteKey = scriptTag?.getAttribute('segment-prod-write-key');
+const segmentProductionWriteKey = scriptTag?.getAttribute('segment-prod-write-key') ?? undefined;
 const segmentDevWriteKey = scriptTag?.getAttribute('segment-dev-write-key') ?? undefined;
 const stagingDomain = scriptTag?.getAttribute('staging-domain') ?? undefined;
 const productionDomain = scriptTag?.getAttribute('production-domain') ?? undefined;
 const enableConsentManager = scriptTag?.getAttribute('enable-consent-manager') ?? 'eu';
 const ga4 = scriptTag?.getAttribute('ga4') ?? 'false';
 const fullstory = scriptTag?.getAttribute('fullstory') ?? 'false';
-const isDev = scriptTag?.getAttribute('is-dev') ?? false;
+const isDev = scriptTag?.getAttribute('is-dev') === 'true';
 
-if (segmentProductionWriteKey) {
+if (segmentProductionWriteKey || segmentDevWriteKey) {
   loadSegmentAnalytics(
     segmentProductionWriteKey,
     enableConsentManager as 'true' | 'false' | 'eu',
     segmentDevWriteKey,
     stagingDomain,
-    productionDomain
+    productionDomain,
+    isDev
   );
 }
 const enabledStandaloneTools: string[] = [];
-if (!segmentProductionWriteKey) {
+if (!segmentProductionWriteKey && !segmentDevWriteKey) {
   if (ga4 !== 'false') {
     initiateGA4();
     enabledStandaloneTools.push('ga4');
@@ -41,6 +42,6 @@ if (!segmentProductionWriteKey) {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  segmentTrackingCode(isDev === 'true' ? true : false);
+  segmentTrackingCode(isDev);
 });
 //window.addEventListener('load', jotformTrackingCode); //sunsetting jotform tracking
